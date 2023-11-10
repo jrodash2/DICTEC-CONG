c88be5edca4f4eb83cbd74a40b312ce373fa68a2
jrodash2 <61074808+jrodash2@users.noreply.github.com>
2023-11-10 10:56:31 -0600

    Funcionalidad de Vistas de Procesos y archivo historico
    se crearon las funcionalidades de las vistas de los procesos funcionalidad de editar, eliminar y enviar ha archivo.

--- dictec/dictec/views.py
+++ dictec/dictec/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import DictamenForm
 from .models import Dictamenfinal
+from django.utils import timezone
 
 # Create your views here.
 
@@ -41,36 +42,28 @@ def home(request):
     return render(request, 'home.html')
 
 def dictamen(request):
-    dictamens = Dictamenfinal.objects.all()
+    dictamens = Dictamenfinal.objects.filter(user=request.user, Datecompleted__isnull=True)
     return render(request, 'dictamen.html', {'dictamens': dictamens})
 
 def dictamen_creado(request):
-    dictamens = Dictamenfinal.objects.all()
+    dictamens = Dictamenfinal.objects.filter(user=request.user, Datecompleted__isnull=True)
     return render(request, 'dictamen_creado.html', {'dictamens': dictamens})
 
 def dictamen_pendiente(request):
-    dictamens = Dictamenfinal.objects.all()
+    dictamens = Dictamenfinal.objects.filter(user=request.user, Datecompleted__isnull=True)
     return render(request, 'dictamen_pendiente.html', {'dictamens': dictamens})
 
 def dictamen_imprimir(request):
-    dictamens = Dictamenfinal.objects.all()
+    dictamens = Dictamenfinal.objects.filter(user=request.user, Datecompleted__isnull=True)
     return render(request, 'dictamen_imprimir.html', {'dictamens': dictamens})
 
+def dictamen_finalizado(request):
+    dictamens = Dictamenfinal.objects.filter(user=request.user, Datecompleted__isnull=True)
+    return render(request, 'dictamen_finalizado.html', {'dictamens': dictamens})
 
-def create_dictamen(request):
-    
-    if request.method == 'GET':
-        return render(request, 'crear_dictamen.html', {
-            'form': DictamenForm
-        })
-    else:
-        form = DictamenForm(request.POST)
-        new_dic = form.save(commit=False)
-        new_dic.user = request.user
-        new_dic.save()
-        return redirect('dictamen')
-       
-    
+
+
+
 
 def dicdetalle(request, dic_id):
     if request.method == 'GET':
@@ -82,7 +75,19 @@ def dicdetalle(request, dic_id):
         form = DictamenForm(request.POST, instance=dictamen)
         form.save()
         return redirect('dictamen')
-
+    
+def complete_dic(request, dic_id):
+     dictamen = get_object_or_404(Dictamenfinal, pk=dic_id)
+     if request.method == 'POST':
+         dictamen.Datecompleted = timezone.now()
+         dictamen.save()
+         return redirect('dictamen')
+    
+def deleted_dic(request, dic_id):
+    dictamen = get_object_or_404(Dictamenfinal, pk=dic_id)
+    if request.method == 'POST':
+        dictamen.delete()
+        return redirect('dictamen')
 
 def editdictamen(request, dic_id):
     if request.method == 'GET':
@@ -98,6 +103,8 @@ def editdictamen(request, dic_id):
         except: ValueError
         return render(request, 'editdictamen.html', {'dictamen': dictamen, 'form': form, 'error': "Error actualiando Dictamen"})
     
+
+
 def dashboard(request):
     return render(request, 'dashboard.html')
 
@@ -125,4 +132,19 @@ def signin(request):
             login(request, user)
             return redirect('dashboard')
 
+
+
+def create_dictamen(request):
+    
+    if request.method == 'GET':
+        return render(request, 'crear_dictamen.html', {
+            'form': DictamenForm
+        })
+    else:
+        form = DictamenForm(request.POST)
+        new_dic = form.save(commit=False)
+        new_dic.user = request.user
+        new_dic.save()
+        return redirect('dictamen')
        
+
