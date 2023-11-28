863638aacb410955920dc3a82596e85107ec0107
jrodash2 <61074808+jrodash2@users.noreply.github.com>
2023-11-28 14:27:37 -0600

    Tailwind CSS
    Aplicación a vistas tecnicos

--- dictec/dictec/forms.py
+++ dictec/dictec/forms.py
@@ -24,4 +24,4 @@ class RespaldoForm(ModelForm):
 class RevisionForm(ModelForm):
     class Meta:
         model = Dictamenfinal 
-        fields = ['Titulo', 'IDuser', 'Nombre', 'Oficina', 'Direccion', 'Tipo_Equipo', 'Marca', 'Modelo', 'Num_Serie', 'Caracteristicas', 'Problema', 'Analisis', 'Proceso_Realizado', 'Revision', 'Dictamen_Final']
+        fields = ['Titulo', 'IDuser', 'Nombre', 'Oficina', 'Direccion', 'Tipo_Equipo', 'Marca', 'Modelo', 'Num_Serie', 'Caracteristicas', 'Problema', 'Analisis', 'Proceso_Realizado', 'Dictamen_Final', 'Revision']
